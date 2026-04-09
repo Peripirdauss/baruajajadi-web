98a711da340e2cc3dceee8f29ba48ef09de511f7
Peripirdauss <99198264+Peripirdauss@users.noreply.github.com>
2026-04-09 11:00:57 +0700

    Fix JSX syntax errors in cta.tsx

--- components/cta.tsx
+++ components/cta.tsx
@@ -7,13 +7,13 @@ export function CTA() {
           <section className="relative py-24 sm:py-32 overflow-hidden">
                 <div className="mx-auto max-w-4xl px-6 lg:px-8">
                         <div className="rounded-[3rem] p-20 text-center">
-                                  <h2 className="text-5xl font-black">Ready to Ascend?</h2>h2>
+                                  <h2 className="text-5xl font-black">Ready to Ascend?</h2>
                                   <div className="flex gap-4 justify-center">
-                                              <Button asChild><Link href="/signup">Get Started <ArrowRight /></Link>Link></Button>Button>
+                                              <Button asChild><Link href="/signup">Get Started <ArrowRight /></Link>Link></Buttonon>
                                   </div>div>
                         </div>div>
                 </div>div>
           </section>section>
-        );
+        
 }
-</section>
+</sect
